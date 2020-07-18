5be97188dd21967df67f6f389bba489bc8d3ca02
Heszler András <andras.heszler@users.noreply.github.com>
2020-07-19 00:07:02 +0200

    Solve the task

--- day5/day5.py
+++ day5/day5.py
@@ -1,3 +1,21 @@
 """cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For
 example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4. """
 
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(p):
+    def first(a, b):
+        return a
+    return p(first)
+
+def cdr(p):
+    def last(a, b):
+        return b
+    return p(last)
+
+
+print(car(cons(3, 4)))
+print(cdr(cons(3, 4)))
